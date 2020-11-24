29b45fd0e635f8fb4307c9cfe7f15cca72a96aaf
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-11-24 14:42:30 +0200

    Starting with Fire Base

--- app/src/main/java/com/example/tournameapp/MainActivity.java
+++ app/src/main/java/com/example/tournameapp/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,9 +17,15 @@ public class MainActivity extends AppCompatActivity {
         String user = "aaaa";
         String pass = "123";
 
-        Intent intent = new Intent(this,LoginActivity.class);
-        intent.putExtra("Username",user);
-        intent.putExtra("Password",pass);
-        startActivity(intent);
+//        Intent intent = new Intent(this,LoginActivity.class);
+//        intent.putExtra("Username",user);
+//        intent.putExtra("Password",pass);
+//        startActivity(intent);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
     }
 }
