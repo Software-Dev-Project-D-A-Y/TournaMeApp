5e5ba1e6eba4d38193488f10b4dd422c2a9ce1e0
Yishay Garame <54310381+YishayGarame@users.noreply.github.com>
2020-12-02 20:00:33 +0200

    LoginPresenter

--- app/src/main/java/com/example/tournameapp/database/UsersService.java
+++ app/src/main/java/com/example/tournameapp/database/UsersService.java
@@ -14,35 +14,36 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UsersService {
 
     private final static String USERS = "Users";
     private final static String USER_NAMES = "Usernames";
     private final static String EMAILS = "Emails";
-    private final static String PLAYERS = "Players";
-    private final static String MANAGERS = "Managers";
+    public final static String PLAYERS = "Players";
+    public final static String MANAGERS = "Managers";
 
     private FirebaseDatabase database;
     private DatabaseReference dbRef;
 
 
-    private ArrayList<String> userNames;
-    private ArrayList<String> emails;
+    private HashMap<String,String> userNames;
+    private HashMap<String,String> emails;
 
     public UsersService() {
         database = FirebaseDatabase.getInstance();
         dbRef = database.getReference(USERS);
 
-        userNames = new ArrayList<>();
-        emails = new ArrayList<>();
+        userNames = new HashMap<>();
+        emails = new HashMap<>();
         dbRef.child(USER_NAMES).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String userName = snapshot.getKey();
+                String type = snapshot.getValue(String.class);
                 Log.d("USER_NAMES.onChildAdded", userName);
-                userNames.add(userName);
+                userNames.put(userName,type);
             }
 
             @Override
@@ -69,8 +70,9 @@ public class UsersService {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 String email = snapshot.getValue(String.class);
+                String userName = snapshot.getKey();
                 Log.d("EMAILS.onChildAdded", email);
-                emails.add(email);
+                emails.put(userName,email);
             }
 
             @Override
@@ -98,7 +100,7 @@ public class UsersService {
     public boolean insertUser(Manager newManager) {
         Log.d("Insert user", "newManager");
         dbRef.child(MANAGERS).child(newManager.getUserName()).setValue(newManager);
-        dbRef.child(USER_NAMES).child(newManager.getUserName()).setValue("Manager");
+        dbRef.child(USER_NAMES).child(newManager.getUserName()).setValue(MANAGERS);
         dbRef.child(EMAILS).child(newManager.getUserName()).setValue(newManager.getEmail());
         return true;
     }
@@ -106,17 +108,55 @@ public class UsersService {
     public boolean insertUser(Player newPlayer) {
         Log.d("Insert user", "newPlayer");
         dbRef.child(PLAYERS).child(newPlayer.getUserName()).setValue(newPlayer);
-        dbRef.child(USER_NAMES).child(newPlayer.getUserName()).setValue("Player");
+        dbRef.child(USER_NAMES).child(newPlayer.getUserName()).setValue(PLAYERS);
         dbRef.child(EMAILS).child(newPlayer.getUserName()).setValue(newPlayer.getEmail());
         return true;
     }
 
     public boolean isEmailExists(String email) {
-        return emails.contains(email);
+        return emails.containsValue(email);
     }
 
     public boolean isUsernameExists(String username) {
         Log.d("userNames", userNames.toString());
-        return userNames.contains(username);
+        return userNames.containsKey(username);
+    }
+
+//    public boolean isPasswordMatch(String userName, String password)
+//    {
+//        String type = getUserType(userName);
+//        switch (type){
+//            case MANAGERS:
+//                dbRef.child(type).child(userName)).setValue(newPlayer);
+//                break; ;
+//            case PLAYERS:
+//                break; ;
+//
+//    }
+//    }
+
+    public String getUserType(String userName)
+    {
+        return userNames.get(userName);
+    }
+
+    public Manager getManager(String userName, String password) {
+        dbRef.child(MANAGERS).child(userName).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Manager manager = snapshot.getValue(Manager.class);
+                manager.getEmail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
+        return null;
+    }
+
+    public Player getPlayer(String userName, String password) {
+        return null;
     }
 }
--- app/src/main/java/com/example/tournameapp/utils/LoginPresenter.java
+++ app/src/main/java/com/example/tournameapp/utils/LoginPresenter.java
@@ -1,15 +1,37 @@
 package com.example.tournameapp.utils;
 
+import com.example.tournameapp.database.UsersService;
 import com.example.tournameapp.interfaces.LoginListener;
+import com.example.tournameapp.model.Manager;
+import com.example.tournameapp.model.Player;
 
 public class LoginPresenter {
 
    private LoginListener loginListener;
+    private UsersService usersService;
 
     public LoginPresenter(LoginListener loginListener) {
         this.loginListener = loginListener;
+        usersService = new UsersService();
     }
     public void logIn(String userName, String password, boolean rememberMe){
 
+        boolean isExists = usersService.isUsernameExists(userName);
+        if(!isExists){
+            //validation
+           // boolean isCorrect = usersService.isPasswordMatch(userName,password);
+        }
+        String type = usersService.getUserType(userName);
+        switch (type)
+        {
+            case  UsersService.MANAGERS:
+               Manager manager = usersService.getManager(userName,password);
+                break;
+
+            case  UsersService.PLAYERS:
+                Player player = usersService.getPlayer(userName,password);
+
+                break;
+        }
     }
 }
